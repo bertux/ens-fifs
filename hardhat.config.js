1d5605cd0b32a248186670ae1eba1c2276be40ed
Bertrand Juglas <b@juglas.name>
2023-12-28 23:15:24 +0100

    add Fuse networks and private key import

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,26 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+// To import private key from .env
+const fs = require("fs");
+const privateKey = fs.readFileSync(".env").toString().trim() || "";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "spark",
+  networks: {
+    spark: {
+      chainId: 123,
+      url: "https://rpc.fusespark.io",
+      accounts: [privateKey],
+      gasPrice: 1000000000,
+    },
+    fuse: {
+      url: "https://rpc.fuse.io",
+      chainId: 122,
+      accounts: [privateKey],
+      gasPrice: 1000000000,
+    },
+  },
   solidity: "0.8.17",
 };
